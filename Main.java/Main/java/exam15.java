12329083e82a810794a85a9832904264a65bd916
이경준 <kyoungjun7777@naver.com>
2025-06-10 21:47:08 +0900

    2025-06-10

--- Main.java/Main/java/exam15.java
+++ Main.java/Main/java/exam15.java
@@ -16,7 +16,7 @@ public class exam15 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(br.readLine());
 
-        String result = (((year%4 == 0 && year%100 != 0) || (year%400 == 0)) ? "1" : "0");
-        System.out.println(result);
+      
+        System.out.println((((year%4 == 0 && year%100 != 0) || (year%400 == 0)) ? "1" : "0"));
     }   
 }
