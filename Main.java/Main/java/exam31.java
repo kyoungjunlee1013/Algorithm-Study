b2d9701a3bd21f6ac88815d60ec9e07731c8bf56
이경준 <kyoungjun7777@naver.com>
2025-06-23 23:40:02 +0900

    2025-06-23

---
+++ Main.java/Main/java/exam31.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class exam31 {
+    public static void main(String[] args) throws IOException{
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력
+// 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 입력의 마지막에는 0 두 개가 들어온다.
+// 출력
+// 각 테스트 케이스마다 A+B를 출력한다.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){            
+            String ar[] = br.readLine().split(" ");
+            int A = Integer.parseInt(ar[0]);
+            int B = Integer.parseInt(ar[1]);        
+                if(A == 0 && B == 0){
+                    break;
+                }            
+                
+                System.out.println(A+B);
+            }
+
+
+    }
+    
+}
