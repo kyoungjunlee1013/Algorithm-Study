c5ad937e0ba0643cd06c339ff62fe49fe94faf62
이경준 <kyoungjun7777@naver.com>
2025-07-09 03:00:20 +0900

    미완료

--- Main.java/Main/java/exam48.java
+++ Main.java/Main/java/exam48.java
@@ -3,25 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class exam48 {
-    public static void main(String[] args) throws IOException{
-//         문제
-// 알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
-// 입력
-// 첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
-// 출력
-// 각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
-// 만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
-        int N = S.length();
 
-        for(int i=0; i<N; i++){
-            char M = S.charAt(i);
-            
-            System.out.println(M);
+        int[] alphabet = new int[26]; // 알파벳 위치 저장용
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = -1; // 초기값은 모두 -1로 설정
         }
 
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            int index = ch - 'a';
+
+            // 처음 나오는 문자일 경우에만 저장
+            if (alphabet[index] == -1) {
+                alphabet[index] = i;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            System.out.print(alphabet[i] + " ");
+        }
     }
-     
 }
